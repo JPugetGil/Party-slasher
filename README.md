4839c94c3e373aa466718b6d14366f4d0cf10b96
Jey Puget Gil <LinkJerome@gmail.com>
2023-05-24 23:15:05 +0200

    Game published

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Party-slasher
 You have to kill someone and find your killer !
+
+## Installation des bibliothèques requises
+Ce guide explique comment installer les bibliothèques requises pour un projet à l'aide de pip, le gestionnaire de packages Python.
+
+### Prérequis
+Python (version 3.x recommandée) est installé sur votre système.
+Assurez-vous de disposer de la version correcte en exécutant la commande suivante dans votre terminal :
+```bash
+python --version
+```
+### Instructions d'installation
+1) Clonez ou téléchargez le dépôt du projet sur votre machine locale.
+2) Ouvrez votre terminal et accédez au répertoire du projet.
+3) Créez un environnement virtuel (facultatif, mais fortement recommandé) en exécutant la commande suivante :
+
+```bash
+python -m venv venv
+```
+Activez l'environnement virtuel. La commande varie selon le système d'exploitation :
+
+Sur Windows :
+```bash
+venv\Scripts\activate
+```
+Sur macOS et Linux :
+```bash
+source venv/bin/activate
+```
+
+Une fois que votre environnement virtuel est actif, vous pouvez procéder à l'installation des bibliothèques requises.
+Assurez-vous que vous êtes toujours dans le répertoire du projet.
+
+Installez les bibliothèques requises en exécutant la commande suivante :
+
+```bash
+pip install -r requirements.txt
+```
+Cette commande va lire le fichier requirements.txt présent dans le répertoire du projet et installer toutes les bibliothèques répertoriées avec leurs dépendances.
+Attendez que pip télécharge et installe toutes les bibliothèques spécifiées.
+Ce processus peut prendre un certain temps en fonction du nombre et de la taille des bibliothèques.
+Une fois l'installation terminée, vous pouvez exécuter le projet avec toutes ses dépendances correctement installées.
+
+### Configurer la partie
+1) Ouvrez le fichier `config-email.json` avec votre éditeur de texte favori
+2) Remplacez les valeurs des champs `email` et `password` par votre adresse email et votre mot de passe
+   Attention, pour Google, pensez à [générer un mot de passe d'application](https://support.google.com/accounts/answer/185833?hl=fr).
+   Si vous utilisez un autre fournisseur, veuillez modifier les champs `smtp_address` et `smtp_port` en fonction de votre fournisseur.
+3) Enregistrez le fichier
+4) Ouvrez le fichier `config.json` avec votre éditeur de texte favori
+5) Définissez l'ensemble des paramètres de la partie (joueurs et joueuses, ainsi que leur mail -crucial pour qu'iels reçoivent les informations-)
+6) Enregistrez le fichier
+
+### Instructions d'exécution
+1) À partir du terminal précédemment ouvert et configuré, exécutez la commande suivante :
+```bash
+python3 script.py
+```
+2) Demandez à vous ami·es de vérifier leurs mails et de suivre les instructions
+3) Enjoy !
+
+> Pensez à commenter l'ensemble des print() dans le code si vous ne voulez pas que les joueurs et joueuses voient les informations de debug (comme les mails des autres joueurs et joueuses ou encore les informations sur la partie)
--- script.py
+++ script.py
@@ -2,6 +2,10 @@ import sys
 import os
 import json
 import random
+import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def assert_exists(file_path):
     if not os.path.exists(file_path):
@@ -10,10 +14,8 @@ def assert_exists(file_path):
 
 # read config json from file path
 def read_config(file_path):
-    if (file_path == None):
-        file_path = "./config.json"
     assert_exists(file_path)
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding="utf8") as f:
         return json.load(f)
     
 def has_one_matched_with_himself(victims, killers):
@@ -27,14 +29,9 @@ def shuffle(victims):
     killers = victims.copy()
     random.shuffle(killers)
     while has_one_matched_with_himself(victims, killers):
-        print("Bad shuffle:")
-        print(killers)
-        print(victims)
-        print("a person cannot be matched with himself. Shuffling again.")
+        print("Bad shuffle: a person cannot be matched with himself. Shuffling again.")
         random.shuffle(killers)
-    print("Good shuffle:")
-    print(killers)
-    print(victims)
+    print("Ended shuffle.")
     return killers, victims
 
 # assign a symbol to each killer
@@ -42,7 +39,7 @@ def assign_symbol(killers, victims, symbols):
     random.shuffle(symbols)
     story = []
     for i in range(len(killers)):
-        print(f'{killers[i]} kills {victims[i]} with {symbols[i]}')
+        # print(f'{killers[i]} kills {victims[i]} with {symbols[i]}')
         story.append({
             "killer": killers[i],
             "victim": victims[i],
@@ -50,19 +47,66 @@ def assign_symbol(killers, victims, symbols):
         })
     return story
 
-# send emails to each killer and victim
-def send_emails(story):
+def send_email(to, subj, body):
+    """Sends an email."""
+    config = read_config("./config-email.json")
+    smtp_address = config["smtp_address"]
+    smtp_port = config["smtp_port"]
+    email_address = config["email_address"]
+    email_password = config["email_password"]
+
+    # Send the mail
+    message = MIMEMultipart('alternative')
+    message['Subject'] = subj
+    message['From'] = email_address
+    message['To'] = to
+    html_part = MIMEText(body, 'html', 'utf-8')
+    message.attach(html_part)
+
+    with smtplib.SMTP_SSL(smtp_address, smtp_port, context=ssl.create_default_context()) as server:
+        # connexion au compte
+        server.login(email_address, email_password)
+        # envoi du mail
+        server.sendmail(email_address, to, message.as_string())
+
+def send_template_email(template, to, subj, **kwargs):
+    """Sends an email using a template."""
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template(template)
+    send_email(to, subj, template.render(**kwargs))
+
+def tell_the_story(story, lieu):
+    """Sends an email to each killer and victim."""
     for i in range(len(story)):
-        print(f"Sending email to {story[i]['killer']['name']} ({story[i]['killer']['email']}) : you will kill {story[i]['victim']['name']} with {story[i]['weapon']}")
-        print(f"Sending email to {story[i]['victim']['name']} ({story[i]['victim']['email']}) : {story[i]['weapon']} will kill you")
-        print("\n")
+        send_template_email(
+            template='criminal.html',
+            to=story[i]['killer']['email'],
+            subj='Mission de haute importance',
+            criminel=story[i]['killer']['name'],
+            victime=story[i]['victim']['name'],
+            arme=story[i]['weapon'],
+            lieu=lieu
+        )
+        # print(f"Sending email to {story[i]['killer']['name']} ({story[i]['killer']['email']}) : you will kill {story[i]['victim']['name']} with {story[i]['weapon']}")
+
+        send_template_email(
+            template='victim.html',
+            to=story[i]['victim']['email'],
+            subj='Attention à vous !',
+            victime=story[i]['victim']['name'],
+            arme=story[i]['weapon']
+        )
+        # print(f"Sending email to {story[i]['victim']['name']} ({story[i]['victim']['email']}) : the sign {story[i]['weapon']} will kill you")
 
 def main():
     # read config
-    config = read_config(sys.argv[1] if len(sys.argv) > 1 else None)
+    config = read_config("./config.json")
     killers, victims = shuffle(config["users"])
     story = assign_symbol(killers, victims, config["symbols"])
-    send_emails(story)
+    tell_the_story(story, config["lieu"])
 
 if __name__ == "__main__":
     main()
